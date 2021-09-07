550710daf8230cdda80595a84c42489e52ba73be
Desislava Todorova <desi_14_94@abv.bg>
2021-09-07 19:41:22 +0300

    more modify

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,18 @@ const router = require('express').Router();
 router.get('/register', (req, res)=>{
     res.render('register', {title: 'Register'});
 });
+router.post('/register', (req, res)=>{
+    console.log(req.body);
+    res.redirect('/auth/register')
+})
+
 
 router.get('/login', (req, res)=>{
     res.render('login', {title: 'Login'})
 })
+router.post('/login', (req, res)=>{
+    console.log(req.body);
+    res.redirect('/auth/login')
+})
+
 module.exports = router;
