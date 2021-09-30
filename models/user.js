4f1053ff4e3c04e635aa9886e78d733b090aab3f
Desislava Todorova <desi_14_94@abv.bg>
2021-09-30 15:26:09 +0300

    .

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
 
 const schema = new Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, },
   hashedPassword: { type: String, required: true },
 });
 
